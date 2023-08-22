00e03e21aaded39efc39ae19cd4ebaa98cc2b689
AndheboinaVijayKumarYadav <andheboinavijaykumaryadav@gmail.com>
2023-08-22 12:15:01 +0530

    Added script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+// variables
+const links = document.querySelectorAll('.nav-list li a');
+
+/* console.log(links); */
+
+for(let link of links){
+
+  /*   console.log(link.getAttribute("href")) */
+    link.addEventListener('click',smoothScroll);
+}
+
+function smoothScroll(e){
+    e.preventDefault();
+
+    const href = this.getAttribute('href');
+
+    // used scrollIntoView Web API
+    document.querySelector(href).scrollIntoView({
+        behavior: 'smooth',
+    });
+
+}
